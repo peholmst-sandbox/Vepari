aa6f854c2ffde515dd15a37ca496221b3cbb73b6
Petter Holmström <petter.holmstrom@outlook.com>
2018-06-24 22:14:09 +0300

    Configuration fix

--- JavaModules/VepariServer/src/main/java/net/pkhapps/vepari/server/domain/DomainModelConfiguration.java
+++ JavaModules/VepariServer/src/main/java/net/pkhapps/vepari/server/domain/DomainModelConfiguration.java
@@ -3,12 +3,14 @@ package net.pkhapps.vepari.server.domain;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Configuration for the domain model.
  */
 @Configuration
 @EntityScan
+@EnableJpaRepositories
 @ComponentScan
 public class DomainModelConfiguration {
 }
